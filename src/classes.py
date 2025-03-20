e67b3e650897d551150c09263879daa4587651af
PeterHartmann <peter.helle.hartmann@gmail.com>
2025-03-20 23:21:52 +0100

    finish most essential code

--- src/classes.py
+++ src/classes.py
@@ -1,22 +1,31 @@
-from sys import stderr
-from pandas import DataFrame, read_excel
-
+import os
+from concurrent.futures import ProcessPoolExecutor, as_completed
+import sys
+from pandas import DataFrame, ExcelWriter, read_excel
+import weasyprint
 import style
-import queue
+from PyPDF2 import PdfReader
+from PyPDF2.errors import PdfReadError
+import requests
 
 
 class ExcelReader:
 
-    def __init__(self, file_path) -> None:
+    def __init__(self, file_path, limit) -> None:
         self.file_path = file_path
-        self.data = None
+        self.limit = limit
         pass
 
     def read(self, index_col="BRnum"):
         try:
-            self.data = read_excel(self.file_path, sheet_name=0, index_col=index_col)
+            return read_excel(
+                self.file_path,
+                sheet_name=0,
+                index_col=index_col,
+                nrows=self.limit,
+            )
         except FileNotFoundError as e:
-            stderr.write(
+            sys.stderr.write(
                 style.fail(
                     f"Failed to read rapport data from file: {e.strerror}: {e.filename}\n"
                 )
@@ -24,55 +33,143 @@ class ExcelReader:
             raise e
 
 
-class Publisher:
-    def __init__(self):
-        self._message_queue = queue.Queue()
-        self._subscribers = []
-
-    def subscribe(self, subscriber):
-        self._subscribers.append(subscriber)
-
-    def publish(self, message):
-        self._message_queue.put(message)
-        for subscriber in self._subscribers:
-            subscriber.receive(message)
-
-
-class Subscriber:
-
-    def receive(self, message):
-        pass
-
-
-class RapportDownloader(Publisher):
+class RapportDownloader:
 
     def __init__(
         self,
-        data: DataFrame,
         out_dir: str = "temp",
-        limit: int = 10,
     ):
-        super().__init__()
         self.out_dir = out_dir
-        self.limit = limit
-        self.data = data
         pass
 
-    def download(self):
-        for brnum in self.data[: self.limit]:
-            file_path = f"{self.out_dir}/{str(brnum)}.pdf"
-            self.publish(brnum)
+    def ensure_output_dir(self) -> None:
+        os.makedirs(self.out_dir, exist_ok=True)
+
+    @staticmethod
+    def process_single_entry(
+        brnum: int, pdf_url: str, html_address: str, out_dir: str
+    ) -> None:
+        file_path = os.path.join(out_dir, f"{brnum}.pdf")
+
+        def validate_downloaded_rapport(file_path) -> bool:
+            if os.path.isfile(file_path):
+                try:
+                    pdfReader = PdfReader(open(file_path, "rb"))
+                    return len(pdfReader.pages) > 0
+                except PdfReadError as e:
+                    print(style.warn(f"{brnum} - PDF reader Error: {e}"), flush=True)
+                    return False
+            return False
+
+        # Attempt to download from Pdf_URL
+        try:
+            response = requests.get(pdf_url, timeout=10, stream=True)
+            response.raise_for_status()
+            with open(file_path, "wb") as out_file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    out_file.write(chunk)
+
+            if validate_downloaded_rapport(file_path):
+                print(style.success(f"{brnum} downloaded from Pdf_URL"), flush=True)
+                return
+        except Exception as e:
+            print(style.warn(f"{brnum} - Download Error for Pdf_URL: {e}"), flush=True)
+            pass
+
+        # If PDF download from "Pdf_URL" fails, try HTML link
+        if isinstance(html_address, str):
+            try:
+                weasyprint.HTML(html_address).write_pdf(file_path)
+                if validate_downloaded_rapport(file_path):
+                    print(
+                        style.success(
+                            f"{brnum} downloaded from Report Html Address",
+                        ),
+                        flush=True,
+                    )
+                    return
+            except Exception as e:
+                print(
+                    style.warn(
+                        f"{brnum} - Download Error for Report Html Address: {e}"
+                    ),
+                    flush=True,
+                )
+                pass  # Log error if needed
+
+    def parallel_download(self, data: DataFrame, max_workers: int = None) -> None:
+        """
+        Downloads reports in parallel using multiple worker processes.
+
+        Args:
+            max_workers (int, optional): Number of parallel processes to use. Defaults to the number of CPU cores.
+        """
+
+        self.ensure_output_dir()
+
+        # If max_workers is not specified, use all available CPU cores
+        if max_workers is None:
+            max_workers = (
+                os.cpu_count() - 1 or 4
+            )  # Fallback to 4 if os.cpu_count() returns None
+
+        # Create a ProcessPoolExecutor with the specified number of workers
+        with ProcessPoolExecutor(max_workers=max_workers) as executor:
+            futures = {
+                executor.submit(
+                    self.process_single_entry,
+                    brnum,
+                    data.at[brnum, "Pdf_URL"],
+                    data.at[brnum, "Report Html Address"],
+                    self.out_dir,
+                ): brnum
+                for brnum in data.index
+            }
+
+            for future in as_completed(futures):  # Allows real-time output
+                try:
+                    future.result()  # Ensures exceptions are raised in the main process
+                except Exception as e:
+                    print(style.fail(f"Error in process {futures[future]}: {e}"))
+
+    def download_slow(self, data: DataFrame) -> None:
+        for brnum in self.data.index:
+            self.process_single_entry(
+                brnum=brnum,
+                pdf_url=data.at[brnum, "Pdf_URL"],
+                html_address=data.at[brnum, "Report Html Address"],
+                out_dir=self.out_dir,
+            )
 
 
-class Metadata(Subscriber):
+class Metadata:
 
-    def __init__(self, data: DataFrame, out_dir="temp"):
-        super().__init__()
+    def __init__(
+        self, data: DataFrame, out_dir: str, source_file: str, download_output_dir: str
+    ):
         self.out_dir = out_dir
-        self.data = data
-        self.foo = []
+        self.out_file = f"Metadata{source_file[4:-5]}.xlsx"
+        self.metadata = data
+        self.metadata["pdf_downloaded"] = "No"
+        downloaded_files = [
+            file
+            for file in os.listdir(download_output_dir)
+            if os.path.isfile(os.path.join(download_output_dir, file))
+        ]
+        for file in downloaded_files:
+            self.metadata.loc[file[:-4], "pdf_downloaded"] = "Yes"
         pass
 
-    def receive(self, message):
-        print(message)
-        pass
+    def save(self):
+        """
+        Saves collected metadata to a excel file
+        """
+        try:
+            os.makedirs(self.out_dir, exist_ok=True)
+            writer = ExcelWriter(
+                os.path.join(self.out_dir, self.out_file), engine="openpyxl"
+            )
+            self.metadata.to_excel(writer, sheet_name="Sheet1")
+            writer._save()
+        except Exception as e:
+            print(e)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,50 @@
-from classes import ExcelReader, Metadata, RapportDownloader
+import os
+from classes import (
+    ExcelReader,
+    Metadata,
+    RapportDownloader,
+)
+import time
+
+
+def main(limit, multiprocess):
+    start_time = time.time()
+    source_dir = "data"
+    source_file = "GRI_2017_2020.xlsx"
+    download_output_dir = "downloads"
+    metadata_output_dir = "metadata"
+    reader = ExcelReader(os.path.join(source_dir, source_file), limit=limit)
+    data = reader.read()
+    downloader = RapportDownloader(out_dir=download_output_dir)
+    if multiprocess == 1:
+        downloader.parallel_download(data=data, max_workers=12)
+    else:
+        downloader.download_slow()
+    metadata = Metadata(data, metadata_output_dir, source_file, download_output_dir)
+    metadata.save()
+    print(
+        f"{'Parallel execution time' if multiprocess == 1 else 'Single-threaded execution time'}--- %s seconds ---"
+        % (time.time() - start_time)
+    )
 
 
 if __name__ == "__main__":
-    print("Running main function")
-    reader = ExcelReader("data/GRI_2017_2020.xlsx")
-    reader.read()
-    downloader = RapportDownloader(reader.data)
-    metadata = Metadata(reader.data)
-    downloader.subscribe(metadata)
-    downloader.download()
+    import argparse
+
+    parser = argparse.ArgumentParser(description="PDF Downloader")
+    parser.add_argument(
+        "--limit",
+        metavar="Integer",
+        required=False,
+        type=int,
+        help="Limit the program to a maximum amount of rows to try and download",
+    )
+    parser.add_argument(
+        "--multiprocess",
+        metavar="Integer",
+        required=False,
+        type=int,
+        help="Chose whether or not to run the program in multiprocessesing mode",
+    )
+    args = parser.parse_args()
+    main(limit=args.limit, multiprocess=args.multiprocess)
